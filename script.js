cf27da4758d2201f7b1ef3d2b39db50ccb21d2d3
huseyinkaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2022-12-12 16:14:49 +0300

    fixx

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const likeBtn = document.getElementById("like-btn")
-
+const likeBtn = document.getElementById("like-button")
+const liked = document.getElementById("change")
+const likeCount = document.getElementById("like-count")
+console.log(likeCount)
 likeBtn.addEventListener("click", function () {
+
+
+    liked.classList.toggle("like-btnTwo")
+
+
 
 
 
