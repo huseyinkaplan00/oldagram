ce987680910d7ef7407f8fee2a46a51aaa7481f6
huseyinkaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2022-12-13 03:22:23 +0300

    fix

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
 const likeBtn = document.getElementById("like-button")
 const liked = document.getElementById("change")
 const likeCount = document.getElementById("like-count")
-console.log(likeCount)
-likeBtn.addEventListener("click", function () {
+const img = document.querySelector(".main-photo");
+const icon = document.querySelector(".icon");
+const dmBtn = document.getElementById("dm-icon-a")
 
+
+
+img.addEventListener("dblclick", () => {
+
+
+
+    icon.classList.add("like");
+    setTimeout(() => {
+        icon.classList.remove("like");
+    }, 1200);
 
     liked.classList.toggle("like-btnTwo")
 
+    if (likeCount.innerHTML === "22,492 likes") {
+        likeCount.innerHTML = "22,493 likes"
+        icon.classList.add("like");
+        setTimeout(() => {
+            icon.classList.remove("like");
+        }, 1200);
+
+    } else {
+        likeCount.innerHTML = "22,492 likes"
+    }
+});
+
+
+
+likeBtn.addEventListener("click", function () {
+    liked.classList.toggle("like-btnTwo")
+
+    if (likeCount.innerHTML === "22,492 likes") {
+        likeCount.innerHTML = "22,493 likes"
+        icon.classList.add("like");
+        setTimeout(() => {
+            icon.classList.remove("like");
+        }, 1200);
+
+    } else {
+        likeCount.innerHTML = "22,492 likes"
+    }
+
+
+
+
+
+})
+
+
+
+dmBtn.addEventListener("click", function () {
 
 
 
